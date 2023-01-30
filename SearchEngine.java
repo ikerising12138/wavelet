a45df8ab287c1f35822e5db71784a9083a58856f
ikerising12138 <kaixuan.qu18@gmail.com>
2023-01-30 13:55:50 -0800

    update for lab report

--- SearchEngine.java
+++ SearchEngine.java
@@ -8,28 +8,23 @@ public class SearchEngine {
     public String handleRequest(URI url) {
 
 
+        System.out.println(url.getQuery());
+        int num = 0;
         if (url.getPath().equals("/")) {
-             return String.format("Kay's Search Engine Word List: %d", lst);
+            return String.format("Kay's Number: %d", num);
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("count")) {
+                    num += Integer.parseInt(parameters[1]);
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                }
             }
-        else if (url.getPath().equals("/increment")){
-            
+            return "404 Not Found!";
         }
-        //System.out.println(url.getQuery());
-        // if (url.getPath().equals("/")) {
-        //     return String.format("Kay's Number: %d", num);
-        // } else if (url.getPath().equals("/increment")) {
-        //     num += 1;
-        //     return String.format("Number incremented!");
-        // } else {
-        //     System.out.println("Path: " + url.getPath());
-        //     if (url.getPath().contains("/add")) {
-        //         String[] parameters = url.getQuery().split("=");
-        //         if (parameters[0].equals("count")) {
-        //             num += Integer.parseInt(parameters[1]);
-        //             return String.format("Number increased by %s! It's now %d", parameters[1], num);
-        //         }
-        //     }
-        //     return "404 Not Found!";
-        // }
     }
 }
